ff2ec48660f837e225078e60f78ed0da6814c0f8
bvattikonda <bvattikonda@shruti-Inspiron-1525.(none)>
2012-03-08 23:31:23 -0500

    Iptables

--- scripts/set_iptables.sh
+++ scripts/set_iptables.sh
@@ -4,10 +4,11 @@
 #destination(ap) ---> source (mobile) --->protocol 202 queue 1. (ack with hashes).
 
 if [ $1 == "bs" ]; then
-    sudo iptables -A OUTPUT -p tcp -s 10.10.10.2 -d 10.10.10.1 -j NFQUEUE --queue-num 1
+    sudo iptables -A OUTPUT -p tcp -s 10.0.0.1 -d 10.0.0.2 -j NFQUEUE --queue-num 0
 #    sudo iptables -A FORWARD -p tcp -d 10.10.10.1 -j NFQUEUE --queue-num 1
 elif [ $1 == "m" ]; then
-    sudo iptables -A INPUT -p 201 -s 10.10.10.2 -d 10.10.10.1 -j NFQUEUE --queue-num 1
+    sudo iptables -A INPUT -p 201 -s 10.0.0.1 -d 10.0.0.2 -j NFQUEUE --queue-num 1
+    sudo iptables -A INPUT -p 200 -s 10.0.0.1 -d 10.0.0.2 -j NFQUEUE --queue-num 1
 elif [ $1 == "c" ]; then
     sudo iptables -F
 elif [ $1 == "lo" ]; then
