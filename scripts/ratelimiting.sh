e2a7a9686271f4aa2a9e3de9b7f837867dae0b21
Bhanu Vattikonda <vbhanu@gmail.com>
2012-03-13 15:56:12 -0400

    rate-limiting

--- scripts/ratelimiting.sh
+++ scripts/ratelimiting.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-userfile=$1
+rate_limit=$1
 
-while read line
-    do
-        rate_limit=$line
-        top -b -d 1 > top_$userfile"_"$rate_limit.top&
-        if [ $rate_limit -lt 5 ]
-        then
-            rate_limit=5
-        fi
-        echo "tc qdisc add dev ath3 root handle 1: cbq avpkt 1000 bandwidth 10mbit"
-        tc qdisc add dev ath3 root handle 1: cbq avpkt 1000 bandwidth 10mbit
-        echo "tc class add dev ath3 parent 1: classid 1:1 cbq rate '$rate_limit'kbit allot 1500 prio 5 bounded isolated"
-        tc class add dev ath3 parent 1: classid 1:1 cbq rate $rate_limit"kbit" allot 1500 prio 5 bounded isolated
-        echo "tc filter add dev ath3 parent 1: protocol ip u32 match ip dst 20.0.0.2 flowid 1:1"
-        tc filter add dev ath3 parent 1: protocol ip u32 match ip dst 20.0.0.2 flowid 1:1
-        echo "tc qdisc add dev ath3 parent 1:1 sfq perturb 10"
-        tc qdisc add dev ath3 parent 1:1 sfq perturb 10
-        echo "Running experiment for $user_file $rate_limit"
-        iperf -c 20.0.0.2 -i 1
-        pkill top
-        tc qdisc del dev ath3 root
-    done < $userfile
+top -b -d 1 > top_$rate_limit.top&
+if [ $rate_limit -lt 5 ]
+then
+    rate_limit=5
+fi
+echo "tc qdisc add dev ath3 root handle 1: cbq avpkt 1000 bandwidth 10mbit"
+tc qdisc add dev ath3 root handle 1: cbq avpkt 1000 bandwidth 10mbit
+echo "tc class add dev ath3 parent 1: classid 1:1 cbq rate '$rate_limit'kbit allot 1500 prio 5 bounded isolated"
+tc class add dev ath3 parent 1: classid 1:1 cbq rate $rate_limit"kbit" allot 1500 prio 5 bounded isolated
+echo "tc filter add dev ath3 parent 1: protocol ip u32 match ip dst 20.0.0.2 flowid 1:1"
+tc filter add dev ath3 parent 1: protocol ip u32 match ip dst 20.0.0.2 flowid 1:1
+echo "tc qdisc add dev ath3 parent 1:1 sfq perturb 10"
+tc qdisc add dev ath3 parent 1:1 sfq perturb 10
+echo "Running experiment for $user_file $rate_limit"
+iperf -c 20.0.0.2 -i 1
+pkill top
+tc qdisc del dev ath3 root
